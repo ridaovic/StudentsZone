98b09a200aef8c90a2a6f0f86fb33b42d293bb81
rida <rida@Ridaovish>
2015-05-06 12:24:55 +0200

    first action

---
+++ Projet1/src/pack/Test.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		System.out.println("test");
+		
+
+	}
+
+}
